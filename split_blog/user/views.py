19a4e35c324bfd2e523060264790681d66a0e623
MBB PRESEA AM <mWX1339082@china.huawei.com>
2025-10-10 16:59:42 -0600

    crea tarea

---
+++ split_blog/user/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404
+
+def index(request):
+    return (HttpResponse('bienvenido a la app de user'))
+
+# Create your views here.
